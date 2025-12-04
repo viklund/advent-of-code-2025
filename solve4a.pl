52416597cd09af81091af68314a8b98b876ad234
Johan Viklund <johan.viklund@nbis.se>
2025-12-04 09:18:01 +0100

    [4] Solved 4a

--- solve4a.pl
+++ solve4a.pl
@@ -11,6 +11,41 @@ while (<>) {
     push @map, [split //];
 }
 
-for my $r (@map) {
-    say join ' ', @$r;
+my $DIMR = $#map;
+my $DIMC = $#{ $map[0] };
+
+my $answer = 0;
+for my $ri (0..$#map) {
+    for my $ci (0..$#{$map[$ri]}) {
+        next if $map[$ri][$ci] ne '@';
+        my @adj = generate_adjacent_positions($ri, $ci);
+        my $count=0;
+        for my $adj (@adj) {
+            $count++ if $map[$adj->[0]][$adj->[1]] eq '@';
+        }
+        $answer++ if $count<4;
+    }
+}
+say $answer;
+
+sub generate_adjacent_positions {
+    my ($r,$c) = @_;
+    my @out;
+    for my $ri (-1,0,1) {
+        next if $r + $ri < 0;
+        next if $r + $ri > $DIMR;
+        for my $ci (-1,0,1) {
+            next if $c + $ci < 0;
+            next if $c + $ci > $DIMC;
+            next if $ri == 0 && $ci == 0;
+            push @out, [$r+$ri, $c+$ci];
+        }
+    }
+    return @out;
+}
+
+sub print_map {
+    for my $r (@map) {
+        say join ' ', @$r;
+    }
 }
