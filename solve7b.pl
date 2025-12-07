3585d7efd9f0f8f48e4a2fb16bee3da0d7c1d002
Johan Viklund <johan.viklund@nbis.se>
2025-12-07 11:31:51 +0100

    [7] b) Not full solution

--- solve7b.pl
+++ solve7b.pl
@@ -5,7 +5,9 @@ use warnings;
 
 use feature qw( say );
 
-my $n = 0;
+my %path;
+
+my $MAX = 0;
 my @p;
 while (<>) {
     chomp;
@@ -19,16 +21,64 @@ while (<>) {
         next if $p[$i] eq '.';
         if ($p[$i] eq '|') {
             if ($r[$i] eq '.') {
-                $r[$i] = $p[$i];
+                $r[$i] = '|';
+                push @{$path{ coord($., $i) }}, coord($.-1, $i);
             }
             elsif ($r[$i] eq '^') {
-                $n++;
                 $r[$i-1] = '|';
                 $r[$i+1] = '|';
+
+                push @{$path{ coord($., $i-1) }}, coord($.-1, $i);
+                push @{$path{ coord($., $i+1) }}, coord($.-1, $i);
             }
         }
     }
+    $MAX = $.;
     @p = @r;
 }
 
+#print_path();
+
+my @ENDS = grep /^$MAX,/, keys %path;
+
+my $n = 0;
+for my $end ( sort sorter @ENDS ) {
+    my $c = count_paths( $end );
+    printf "C: %5s  %2d\n", $end, $c;
+    $n += count_paths( $end );
+}
 say $n;
+
+
+sub sorter {
+    my($a1,$a2) = split /,/,$a;
+    my($b1,$b2) = split /,/,$b;
+    return $a1<=>$b1 || $a2<=>$b2;
+}
+
+sub count_paths {
+    my $coord = shift;
+    return 1 unless exists $path{$coord};
+
+    my $up = $path{ $coord };
+    if ( @$up == 1 ) {
+        return count_paths($up->[0]);
+    }
+
+    my $n = 0;
+    for (@$up) {
+        $n += count_paths($_);
+    }
+    return $n;
+}
+
+sub print_path {
+    for my $k (sort sorter keys %path) {
+        printf "    %5s => %s\n", $k, join(' ', map { sprintf "%5s", $_ } @{$path{$k}});
+    }
+}
+
+sub coord {
+    my ($x,$y) = @_;
+    sprintf "%d,%d", $x, $y;
+}
