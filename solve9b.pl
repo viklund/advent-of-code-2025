d273a4440bf4078f755d0274c5aab311dbb2c5fe
Johan Viklund <johan.viklund@nbis.se>
2025-12-09 15:03:34 +0100

    [9] Solved

--- solve9b.pl
+++ solve9b.pl
@@ -12,28 +12,53 @@ while (<>) {
 push @coords, $coords[0]; # Wrap around
 
 my $max = 0;
-TRY:
-for my $ci (0..$#coords-3) {
-    my @cs = @coords[$ci..$ci+3];
-    my $size = (1+abs($cs[0][0] - $cs[2][0])) * (1+abs($cs[0][1] - $cs[2][1]));
-    if ( $size > $max ) {
-        for my $p (@coords) {
-            if (inside($p, $cs[0], $cs[2])) {
-                next TRY;
+for my $ci (0..$#coords-1) {
+    TRY:
+    for my $cj ($ci+1..$#coords) {
+        my @cs = ($coords[$ci], $coords[$cj]);
+        my $size = (1+abs($cs[0][0] - $cs[1][0])) * (1+abs($cs[0][1] - $cs[1][1]));
+        if ( $size > $max ) {
+            for my $pi (0..$#coords-1) {
+                if (line_crossing(@coords[$pi, $pi+1], @cs)) {
+                    next TRY;
+                }
+
             }
+            $max = $size;
         }
-        $max = $size;
     }
 }
-say $max;
+say "$max";
 
-sub inside {
-    my ($p, @cs) = @_;
-    
-    if ( $p->[0] > $cs[0][0] && $p->[0] < $cs[1][0] ) {
-        if ( $p->[1] > $cs[0][1] && $p->[1] < $cs[1][1] ) {
-            return 1;
-        }
+sub line_crossing {
+    my ($l1, $l2, @cs) = @_;
+    my @xs = sort { $a <=> $b } map { $_->[0] } @cs;
+    my @ys = sort { $a <=> $b } map { $_->[1] } @cs;
+    # X coordinate the same, test vertical
+    if ( $l1->[0] == $l2->[0] ) {
+        ## Line is outside on sides
+        return if $l1->[0] <= $xs[0];
+        return if $l1->[0] >= $xs[1];
+
+        my (@ly) = sort { $a <=> $b } ($l1->[1], $l2->[1]);
+        ## Line is outside, up or down
+        return if $ly[1] <= $ys[0];
+        return if $ly[0] >= $ys[1];
+
+        return 1;
     }
-    return '';
+    # Y coordinate the same, test horizontal
+    if ( $l1->[1] == $l2->[1] ) {
+        ## Line is outside above or below
+        return if $l1->[1] <= $ys[0];
+        return if $l1->[1] >= $ys[1];
+
+        my (@lx) = sort { $a <=> $b } ($l1->[0], $l2->[0]);
+        ## Line is outside, left or right
+        return if $lx[1] <= $xs[0];
+        return if $lx[0] >= $xs[1];
+
+        return 1;
+    }
+    return;
 }
